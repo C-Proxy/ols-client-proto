d1f6724ce71d20e1494fdd921bee1b7fdc37c632
C-Proxy <nc2.7182818284590@gmail.com>
2020-06-15 22:02:17 +0900

    Add GammaSlider

---
+++ Assets/Scripts/ViewLayer/GammaSlider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class GammaSlider : MonoBehaviour
+{
+    [SerializeField]
+    Material _Material = default;
+    [SerializeField]
+    Slider _Slider = default;
+
+    private void Awake()
+    {
+        _Slider.OnValueChangedAsObservable()
+        .Subscribe(value => _Material.SetFloat("_Gamma", value)).AddTo(this);
+    }
+
+}
